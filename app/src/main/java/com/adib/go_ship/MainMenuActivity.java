116f8000418ebbef79ad5226f792f13b573a5520
uliadib12 <uliadib@gmail.com>
2023-01-08 16:40:28 +0700

    connect ViewPager with BottomNavigation

--- app/src/main/java/com/adib/go_ship/MainMenuActivity.java
+++ app/src/main/java/com/adib/go_ship/MainMenuActivity.java
@@ -3,10 +3,12 @@ package com.adib.go_ship;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -25,6 +27,39 @@ public class MainMenuActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.viewPager_mainMenu);
         pagerAdapter = new ScreenSlidePagerAdapter(this);
         viewPager.setAdapter(pagerAdapter);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.page_1:
+                    viewPager.setCurrentItem(0);
+                    break;
+                case R.id.page_2:
+                    viewPager.setCurrentItem(1);
+                    break;
+                case R.id.page_3:
+                    viewPager.setCurrentItem(2);
+                    break;
+            }
+            return true;
+        });
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        bottomNavigationView.getMenu().findItem(R.id.page_1).setChecked(true);
+                        break;
+                    case 1:
+                        bottomNavigationView.getMenu().findItem(R.id.page_2).setChecked(true);
+                        break;
+                    case 2:
+                        bottomNavigationView.getMenu().findItem(R.id.page_3).setChecked(true);
+                        break;
+                }
+            }
+        });
     }
 
     private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
